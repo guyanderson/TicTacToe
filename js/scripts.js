0e700e00c06f9e171cc093439aea87fbab5f630d
Anabel Ramirez and guy anderson <anabel+guy@>
2017-05-10 14:07:41 -0700

    return id value when square clicked.Stops, not finished.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@
 // function Player() {
 //   this.playerX = playerX;
 //   this.playerO = playerO;
+      	this.WinPatterns = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
+
 
 
 
@@ -11,21 +13,21 @@ var turn = 0;
 var icon = 'X';
 var playerX = [];
 var playerO = [];
-// var square = parseInt($("div#id").val());
 
 $('.cell').click(function(){
   if($(this).html()==""){
     if (turn%2!==0) {
       icon = 'O';
       $(this)[0].id;
+      playerO.push($(this)[0].id);
     } else {
       icon = 'X';
       $(this)[0].id;
+      playerX.push($(this)[0].id);
     }
     $(this).html(icon);
     // checkWin in here
     turn++
   }
-  console.log($(this)[0].id);
 });
 });
